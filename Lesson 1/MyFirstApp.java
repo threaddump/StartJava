4c3458da8cfae1779cc6796c5a8c644febee38d0
Eugene Logunov <eugene.logunov@gmail.com>
2021-10-01 18:26:12 +0300

    Lesson 1: MyFirstApp (part 2), Variable (part 6)

---
+++ Lesson 1/MyFirstApp.java
@@ -0,0 +1,17 @@
+public class MyFirstApp {
+
+    /*
+        compile: javac -encoding utf8 MyFirstApp.java
+            note: "-encoding utf8" is necessary to display cyrillic letters in console
+        run class: java MyFirstApp
+        decompile class: javap -c -p -s -verbose MyFirstApp.class
+
+        make jar from class file: jar cf MyFirstApp.jar MyFirstApp.class
+        run jar: java -cp MyFirstApp.jar MyFirstApp
+        decompile class from jar: javap -c -p -s -verbose -cp MyFirstApp.jar MyFirstApp
+    */
+    
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+    }
+}
---
+++ Lesson 1/Variable.java
@@ -0,0 +1,36 @@
+/*
+    Урок 1, часть 6:
+
+    Создайте класс Variable с методом main
+    Объявите в методе переменные всех существующих в Java примитивных типов данных. Каждой переменной присвойте значение
+    В качестве значений используйте информацию о своем компьютере: количество ядер, частота процессора и тд.
+    Выведите все значения переменных на консоль
+*/
+public class Variable {
+    
+    public static void main(String[] args) {
+        byte cpuPhysicalCores = 6;
+        System.out.println(cpuPhysicalCores);
+
+        short yearOfAssembly = 2014;
+        System.out.println(yearOfAssembly);
+
+        int psuPower = 600;
+        System.out.println(psuPower);
+
+        long cpuFreqHz = 3400000000L;
+        System.out.println(cpuFreqHz);
+
+        float cpuFreqGhz = 3.4f;
+        System.out.println(cpuFreqGhz);
+
+        double driveCSpaceUsedPct = 63.92857d;
+        System.out.println(driveCSpaceUsedPct);
+
+        char lastDriveLetter = 'H';
+        System.out.println(lastDriveLetter);
+
+        boolean powerOn = true;
+        System.out.println(powerOn);
+    }
+}
