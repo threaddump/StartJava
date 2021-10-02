563faa84faf853353f89280b26fc8ce468264d4b
Eugene Logunov <eugene.logunov@gmail.com>
2021-10-02 18:55:51 +0300

    Lesson 2: H/W (part 6, problem 2) - Jaeger, JaegerTest

---
+++ Lesson 2/Jaeger.java
@@ -0,0 +1,98 @@
+/*
+    Урок 2, часть 6, задача 2
+
+    Модифицируйте класс Jaeger, сделав его универсальным (без инициализации полей в самом классе, как мы это делали в уроке). Это позволит на его основе создавать любого робота:
+        информацию о роботах можно найти здесь: https://pacificrim.fandom.com/wiki/Category:Jaegers
+        выберите два или более разных роботов. Найдите между ними что-то общее (поля, методы). Реализуйте это в коде (не усложняйте реализацию. Делайте все максимально просто)
+        создайте 2 экземпляра класса Jaeger в классе JaegerTest и поэкспериментируйте с ними (проинициализируйте поля, вызывайте методы, перезаписывайте и выводите значения полей)
+        для инициализации полей объектов при их создании используйте в начале сеттеры (покажите решение ментору), а потом перепишите, используя конструктор (1, 2)
+*/
+public class Jaeger {
+
+    // model name: Chronos Berserker
+    // mark: Mark-5
+    // weapons: 2x Hammerhead Missles
+    // status: destroyed
+
+    // model name: Titan Redeemer
+    // mark: Mark-6
+    // weapons: M-19 Morning Star, EMP Mist Grenade, Arm Mounted Cannons
+    // status: destroyed
+
+    private String modelName;
+    private String mark;
+    private String weapon;
+    private boolean operational;
+
+    public Jaeger() {
+        modelName = "N/A";
+        mark = "N/A";
+        weapon = "N/A";
+        operational = true;
+    }
+
+    public Jaeger(String modelName, String mark, String weapon, boolean operational) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.weapon = weapon;
+        this.operational = operational;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    public boolean isOperational() {
+        return operational;
+    }
+
+    public void setOperational(boolean operational) {
+        if (this.operational) {
+            this.operational = operational;
+        }
+    }
+
+    public void walk() {
+        if (operational) {
+            System.out.println(modelName + " is walking");
+        } else {
+            System.out.println(modelName + " is not operational");
+        }
+    }
+
+    public void aim() {
+        if (operational) {
+            System.out.println(modelName + " is aiming");
+        } else {
+            System.out.println(modelName + " is not operational");
+        }
+    }
+
+    public void useWeapon() {
+        if (operational) {
+            System.out.println(modelName + " uses his " + weapon + ". Pew-pew!");
+        } else {
+            System.out.println(modelName + " is not operational");
+        }
+    }
+}
---
+++ Lesson 2/JaegerTest.java
@@ -0,0 +1,56 @@
+/*
+    Урок 2, часть 6, задача 2
+
+    Модифицируйте класс Jaeger, сделав его универсальным (без инициализации полей в самом классе, как мы это делали в уроке). Это позволит на его основе создавать любого робота:
+        информацию о роботах можно найти здесь: https://pacificrim.fandom.com/wiki/Category:Jaegers
+        выберите два или более разных роботов. Найдите между ними что-то общее (поля, методы). Реализуйте это в коде (не усложняйте реализацию. Делайте все максимально просто)
+        создайте 2 экземпляра класса Jaeger в классе JaegerTest и поэкспериментируйте с ними (проинициализируйте поля, вызывайте методы, перезаписывайте и выводите значения полей)
+        для инициализации полей объектов при их создании используйте в начале сеттеры (покажите решение ментору), а потом перепишите, используя конструктор (1, 2)
+
+    javac Jaeger.java JaegerTest.java
+    java JaegerTest
+*/
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        // init fields using setters
+        Jaeger j1 = new Jaeger();
+        j1.setModelName("Chronos Berserker");
+        j1.setMark("Mark-5");
+        j1.setWeapon("2x Hammerhead Missles");
+        j1.setOperational(true);
+
+        // init fields using ctor
+        Jaeger j2 = new Jaeger("Titan Redeemer", "Mark-6", "M-19 Morning Star", false);
+
+        System.out.println(
+                "Jaeger 1: modelName = " + j1.getModelName() +
+                "; mark = " + j1.getMark() +
+                "; weapon = " + j1.getWeapon() +
+                "; operational = " + j1.isOperational()
+                );
+
+        System.out.println(
+                "Jaeger 2: modelName = " + j2.getModelName() +
+                "; mark = " + j2.getMark() +
+                "; weapon = " + j2.getWeapon() +
+                "; operational = " + j2.isOperational()
+                );
+
+        j1.walk();
+        j1.aim();
+        j1.useWeapon();
+        j1.setOperational(false);
+        j1.walk();
+        j1.setOperational(true);
+        j1.useWeapon();
+        j1.setWeapon("N/A");
+
+        System.out.println(
+                "Jaeger 1: modelName = " + j1.getModelName() +
+                "; mark = " + j1.getMark() +
+                "; weapon = " + j1.getWeapon() +
+                "; operational = " + j1.isOperational()
+                );
+    }
+}
