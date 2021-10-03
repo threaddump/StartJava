5c84be814db99ecefd7c233c90d015ffcf93ec15
Eugene Logunov <eugene.logunov@gmail.com>
2021-10-03 14:31:04 +0300

    Lesson 4: completed H/W (part 6, problem 2) - updated Player, GuessNumber, GuessNumberTest

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -19,11 +19,25 @@
             no — игра завершается
             при вводе иного значения выводите сообщение до тех пор, пока не будут введены допустимые ответы
 
+    Урок 4, часть 6, задача 2
+
+    Модифицируйте программу Угадай число:
+        используйте массив для хранения в классе Player, введенных игроком чисел
+        после запуска игры выведите сообщение о том, что у каждого игрока по 10 попыток
+        в конце игры выведите сообщение "Игрок " + имя + " угадал число " + число + " с " + номер + " попытки"
+        если у игрока закончились попытки, то отобразите сообщение: "У " + имя + " закончились попытки"
+        по окончанию игры отобразите, в две строки через пробел, все названные игроками числа (даже если никто не выиграл)
+        если массив полностью не заполнен, т.е. пользователь угадал число менее чем за 10 попыток, то выводить весь массив не нужно
+        для получения части массива используйте метод Arrays.copyOf
+        для обнуления массива, при повторном запуске игры, используйте метод Arrays.fill(). При этом обнуляйте только те ячейки, где хранятся, названные игроками числа, а не весь массив
+        попробуйте улучшить читаемость кода, разделив его на небольшие методы (рефакторинг)
+
     javac -encoding utf8 -d out -sourcepath src src/com/startjava/lesson_2_3_4/game/GuessNumberTest.java
     java -cp out com.startjava.lesson_2_3_4.game.GuessNumberTest
 */
 package com.startjava.lesson_2_3_4.game;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GuessNumber {
@@ -45,33 +59,47 @@ public class GuessNumber {
     }
 
     public boolean isComplete() {
-        return (winner != null);
+        return (winner != null) || !(player1.anyAttemptsLeft() || player2.anyAttemptsLeft());
     }
 
     public void initialize() {
         System.out.println("Компьютер загадал случайное число от 0 до 100. Попробуйте его отгадать!");
+
         secretNumber = (int) (Math.random() * 100.0);
         activePlayer = (Math.random() < 0.5) ? player1 : player2;
+
         winner = null;
+
+        player1.reset();
+        player2.reset();
     }
 
     public void step(Scanner scanner) {
         int userGuess;
         do {
-            System.out.print("Ход игрока " + activePlayer.getName() + ". Введите число от 0 до 100: ");
+            System.out.print(
+                    "Ход игрока " + activePlayer.getName() +
+                    ". Осталось попыток - " + activePlayer.getAttemptsLeft() +
+                    ". Введите число от 0 до 100: "
+                    );
 
             userGuess = scanner.nextInt();
             // skip CR+LF
             scanner.nextLine();
         } while ((userGuess < 0) || (userGuess > 100));
-        activePlayer.setNumber(userGuess);
+
+        activePlayer.storeNumber(userGuess);
+        activePlayer.burnAttempt();
 
         if (userGuess != secretNumber) {
-            // display hint
             if (userGuess < secretNumber) {
                 System.out.println("Данное число меньше того, что загадал компьютер");
             } else /* if (userGuess > secretNumber) */ {
                 System.out.println("Данное число больше того, что загадал компьютер");
+            }
+
+            if (!activePlayer.anyAttemptsLeft()) {
+                System.out.println("У " + activePlayer.getName() + " закончились попытки");
             }
 
             // switch player
@@ -85,7 +113,30 @@ public class GuessNumber {
         }
     }
 
-    public void reportWinner() {
-        System.out.println("Игрок " + winner.getName() + " отгадал число. Поздравляю с победой!");
+    public void reportResults() {
+        System.out.println();
+
+        if (winner != null) {
+            System.out.println(
+                    "Игрок " + winner.getName() +
+                    " угадал число " + secretNumber +
+                    " с " + (Player.MAX_ATTEMPTS - winner.getAttemptsLeft()) + " попытки"
+                    );
+        } else {
+            System.out.println("Раунд завершен. У обоих игроков закончились попытки");
+        }
+
+        displayStoredNumbers(player1);
+        displayStoredNumbers(player2);
+
+        System.out.println();
+    }
+
+    private void displayStoredNumbers(Player player) {
+        int[] numbers = player.getStoredNumbers();
+        if (numbers.length > 0) {
+            System.out.print("Числа, введенные игроком " + player.getName() + ": ");
+            System.out.println(Arrays.toString(numbers));
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/game/Player.java
+++ src/com/startjava/lesson_2_3_4/game/Player.java
@@ -19,26 +19,67 @@
             no — игра завершается
             при вводе иного значения выводите сообщение до тех пор, пока не будут введены допустимые ответы
 
+    Урок 4, часть 6, задача 2
+
+    Модифицируйте программу Угадай число:
+        используйте массив для хранения в классе Player, введенных игроком чисел
+        после запуска игры выведите сообщение о том, что у каждого игрока по 10 попыток
+        в конце игры выведите сообщение "Игрок " + имя + " угадал число " + число + " с " + номер + " попытки"
+        если у игрока закончились попытки, то отобразите сообщение: "У " + имя + " закончились попытки"
+        по окончанию игры отобразите, в две строки через пробел, все названные игроками числа (даже если никто не выиграл)
+        если массив полностью не заполнен, т.е. пользователь угадал число менее чем за 10 попыток, то выводить весь массив не нужно
+        для получения части массива используйте метод Arrays.copyOf
+        для обнуления массива, при повторном запуске игры, используйте метод Arrays.fill(). При этом обнуляйте только те ячейки, где хранятся, названные игроками числа, а не весь массив
+        попробуйте улучшить читаемость кода, разделив его на небольшие методы (рефакторинг)
+
     javac -encoding utf8 -d out -sourcepath src src/com/startjava/lesson_2_3_4/game/GuessNumberTest.java
     java -cp out com.startjava.lesson_2_3_4.game.GuessNumberTest
 */
 package com.startjava.lesson_2_3_4.game;
 
+import java.util.Arrays;
+
 public class Player {
 
+    public static int MAX_ATTEMPTS = 10;
+
     private String name;
-    private int number;
-    
+    private int attemptsLeft;
+    private int[] numbers = new int[MAX_ATTEMPTS];
+
     public Player(String name) {
         this.name = name;
-        number = 0;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public int getAttemptsLeft() { return attemptsLeft; }
+
+    public boolean anyAttemptsLeft() { return attemptsLeft > 0; }
+
+    public void burnAttempt() { attemptsLeft--; }
+
+    public void storeNumber(int number) {
+        numbers[MAX_ATTEMPTS - attemptsLeft] = number;
+    }
+
+    public int[] getStoredNumbers() {
+        return Arrays.copyOf(numbers, MAX_ATTEMPTS - attemptsLeft);
+    }
+
+    public void reset() {
+        Arrays.fill(numbers, 0, MAX_ATTEMPTS - attemptsLeft, 0);
+        attemptsLeft = MAX_ATTEMPTS;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", attemptsLeft=" + attemptsLeft +
+                ", numbers=" + Arrays.toString(numbers) +
+                '}';
     }
 }
